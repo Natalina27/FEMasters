c968330d852dd3d4372d31da6c57f349f5d9698c
natalyamyunster <natalyamyunster@yahoo.com>
2021-01-09 11:47:24 +0100

    09.01.21

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//==== Object , delete , in =======
 const obj = {
     left: 1,
     right: 2
@@ -15,6 +16,7 @@ console.log('left' in obj);
 console.log('right' in obj);
 
 //=================================
+//==== Array , delete , in ========
 
 const array = [11, 22]
 
@@ -33,4 +35,22 @@ console.log(array[0]);
 console.log(0 in array);
 console.log(1 in array);
 console.log(2 in array);
+
+//=================================
+//==== Object , Object.keys =======
+
+console.log('keys', Object.keys(obj));
+//=================================
+//==== Object , Object.assign =====
+const obj1 = {
+    up: 3,
+    down: 4
+};
+console.log(Object.assign(obj, obj1));
+console.log({...obj, ...obj1});
+
+console.log(Object.assign(obj1, obj));
+console.log({...obj1, ...obj});
+
+console.log('keys', Object.keys(obj));
 
